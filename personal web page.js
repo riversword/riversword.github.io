90f63abd3a52655685eafe7ec5728b3c23f3bd54
riversword <2694473261@qq.com>
2017-10-29 11:07:41 +0800

    代码规范化，修复从移动端切换到PC端时导航条被隐藏的bug

--- personal web page.js
+++ personal web page.js
@@ -1,30 +1,25 @@
 $(document).ready(function(){
 	$(window).scroll(function () {  
 
-			//console.log("导航条距顶部的距离"+$(".navbar").offset().top);
-            //console.log($("#home nav .mobileNav").css('display'));
-			//console.log("滚动的距离"+$(document).scrollTop());
-            //PC端固定导航栏
+            //under PC model, when scroll to "about" section, fix the navigation bar to top 
             if ($("#home nav").offset().top<=$(document).scrollTop() && $("#home nav .mobileNav").css('display')==='none' ) {
             	$("#home nav").addClass("fixNav");
             }
-            
+            //change the navigation's background color
+            if($(document).scrollTop()>600){
+                $("#home nav").addClass("bg");
+            }else{
+                $("#home nav").removeClass("bg");
+            }
+
+            //when scroll to "about" section, trigger the hello animation in "about" section
             if($(document).scrollTop()+getViewportHeight()>$('.aboutLeft').offset().top+$('.aboutLeft').height()/2 && $(document).scrollTop()<$('.aboutLeft').offset().top+$('.aboutLeft').height()){
                 $('.aboutLeft div').addClass("hello");
             }else{
                 $('.aboutLeft div').removeClass("hello");
             }
-            //PC端导航栏颜色变化
-            if($(document).scrollTop()>600){
-            	$("#home nav").addClass("bg");
-            }else{
-            	$("#home nav").removeClass("bg");
-            }
-
-            //getElementViewTop($("#works .workshow:eq(0)>div").get(0));
-            //getViewport();
-            //console.log('works1距文档顶部'+$("#works .workshow:eq(0)").offset().top);
-            //导航条高度60，wroks展示模块高300
+            
+            //show the animation in "works" section , 60 is the navigation's height nearly , 300 is the workshow'height
             for(var i=0;i<$("#works .workshow").length;i++){
                 //console.log($("#works .workshow").eq(i).offset().top+50+'是works的距离');
                 if($(document).scrollTop()+getViewportHeight()>$("#works .workshow").eq(i).offset().top+50 && $(document).scrollTop()+60<$("#works .workshow").eq(i).offset().top+300){
@@ -37,13 +32,14 @@ $(document).ready(function(){
             
            
     });
-	//点击导航按钮，页面滚动到目标位置
+
+	//click the button in navigation, then go the section
 	$(".scroll").click(function(event){
         event.preventDefault();
         $("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
     });
 
-	//点击作品生成预览
+	//click the "workshow" in "works" section, show the demo
 	$("#works .describe").click(function(){
 		$(this).parent().children('div.viewWork').removeClass('displayHidden');
 	});
@@ -51,28 +47,34 @@ $(document).ready(function(){
 	$("#works .viewWork>i").click(function(){
 		$(this).parent('div.viewWork').addClass('displayHidden');
 	});
-	//works鼠标悬停动画
+
+	//when the mouse hover the "workshow", show the introduction
 	$("#works .describe").mouseover(function(){
 		$(this).addClass('hoverAntimate');
 	});
 	$("#works .describe").mouseout(function(){
 		$(this).removeClass('hoverAntimate');
 	});
-
-
-    function getViewportHeight(){
-　　　　if (document.compatMode == "BackCompat"){
-               return document.body.clientHeight;
-                //console.log('页面高'+document.body.clientHeight);
-　　　　} else {
-               return document.documentElement.clientHeight;
-                //console.log('页面高'+document.documentElement.clientHeight);
-　　　　}
-　　}
     
-
-    //移动端导航栏
+    //under mobile model, show/hide the navigation when click the menu icon
     $('#home .mobileNav').click(function(){
         $('#home .myNav').toggle();
     });
+
+    //form mobile to PC model, show the navigation bar 
+    $(window).resize(function(){
+        if(document.body.clientWidth > 500){
+            $('#home .myNav').show();
+        }
+    });
+    
+    //get the document's height
+    function getViewportHeight(){
+　　　　if (document.compatMode == "BackCompat"){
+               return document.body.clientHeight;
+　　　　} else {
+               return document.documentElement.clientHeight;
+　　　　}
+　　}
+    
 });
